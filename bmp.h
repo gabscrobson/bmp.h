a44682653cfe68654074b46e1f45553de1a801ab
Gabriel Accetta <gmta1410@gmail.com>
2023-05-02 13:55:05 +0000

    changed bmpin and bpmout

--- bmp.h
+++ bmp.h
@@ -70,86 +70,85 @@ typedef struct
 }
 bmp;
 
-int bmpIn(char *filename, bmp *pt) {
-    bmp result;
+bmp* bmpIn(char *filename) {
+    bmp *result = malloc(sizeof(bmp));
 
     // open file
     FILE *fp = fopen(filename, "r");
     if(fp == NULL) {
         printf("Could not open %s.\n", filename);
-        return 1;
+        return NULL;
     }
 
     // READ FILE HEADER
-    fread(&result.bf, sizeof(BITMAPFILEHEADER), 1, fp);
+    fread(&result->bf, sizeof(BITMAPFILEHEADER), 1, fp);
 
     // READ INFO HEADER
-    fread(&result.bi, sizeof(BITMAPINFOHEADER), 1, fp);
+    fread(&result->bi, sizeof(BITMAPINFOHEADER), 1, fp);
 
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
-    if (result.bf.bfType != 0x4d42 || result.bf.bfOffBits != 54 || result.bi.biSize != 40 ||
-        result.bi.biBitCount != 24 || result.bi.biCompression != 0)
+    if (result->bf.bfType != 0x4d42 || result->bf.bfOffBits != 54 || result->bi.biSize != 40 ||
+        result->bi.biBitCount != 24 || result->bi.biCompression != 0)
     {
         fclose(fp);
         printf("Unsupported file format.\n");
-        return 2;
+        return NULL;
     }
 
     // height and width from info header
-    result.height = abs(result.bi.biHeight);
-    result.width = result.bi.biWidth;
+    result->height = abs(result->bi.biHeight);
+    result->width = result->bi.biWidth;
 
     // allocate memory for a 2D array of pixels
-    // result.pixels = calloc(result.height, result.width * sizeof(RGBTRIPLE)); RGBTRIPLE (*pixelsBuffer)[result.width] = calloc(result.height, result.width * sizeof(RGBTRIPLE));
+    // result->pixels = calloc(result->height, result->width * sizeof(RGBTRIPLE)); RGBTRIPLE (*pixelsBuffer)[result->width] = calloc(result->height, result->width * sizeof(RGBTRIPLE));
 
-    result.pixels = (RGBTRIPLE**)malloc(result.height * sizeof(RGBTRIPLE*));
-    for(int i = 0; i < result.height; i++)
-        result.pixels[i] = (RGBTRIPLE*)malloc(result.width * sizeof(RGBTRIPLE));
+    result->pixels = (RGBTRIPLE**)malloc(result->height * sizeof(RGBTRIPLE*));
+    for(int i = 0; i < result->height; i++)
+        result->pixels[i] = (RGBTRIPLE*)malloc(result->width * sizeof(RGBTRIPLE));
 
-    if (result.pixels == NULL)
+    if (result->pixels == NULL)
     {
         printf("Not enough memory to store image.\n");
         fclose(fp);
-        return 3;
+        return NULL;
     }
 
     // determine padding for scanlines
-    result.padding =  (4 - (result.width * sizeof(RGBTRIPLE)) % 4) % 4;
+    result->padding =  (4 - (result->width * sizeof(RGBTRIPLE)) % 4) % 4;
 
     // iterate over scanlines
-    for (int i = 0; i < result.height; i++)
+    for (int i = 0; i < result->height; i++)
     {
         // read row into pixel array
-        fread(result.pixels[i], sizeof(RGBTRIPLE), result.width, fp);
+        fread(result->pixels[i], sizeof(RGBTRIPLE), result->width, fp);
 
         // skip over padding
-        fseek(fp, result.padding, SEEK_CUR);
+        fseek(fp, result->padding, SEEK_CUR);
     }
 
     fclose(fp);
 
-    *pt = result;
-    return 0;
+    return result;
 }
 
-void bmpOut(char *filename, bmp image) {
+void bmpOut(char *filename, bmp *image) {
 
     FILE * outptr = fopen(filename, "w");
 
     // write BITMAPFILEHEADER
-    fwrite(&image.bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+    fwrite(&image->bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
     // write BITMAPINFOHEADER
-    fwrite(&image.bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+    fwrite(&image->bi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
     // write new pixels to outfile
-    for (int i = 0; i < image.height; i++)
+    for (int i = 0; i < image->height; i++)
     {
         // write row to outfile
-        fwrite(image.pixels[i], sizeof(RGBTRIPLE), image.width, outptr);
+        fwrite(image->pixels[i], sizeof(RGBTRIPLE), image->width, outptr);
 
         // write padding at end of row
-        for (int k = 0; k < image.padding; k++)
+        for (int k = 0; k < image->padding; k++)
         {
             fputc(0x00, outptr);
         }
